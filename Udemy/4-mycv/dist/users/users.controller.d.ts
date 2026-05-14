7771a39b81298d144b1285684fedbe351428c67a
Kuldeepsinh Jhala <kuldeephjhala@gmail.com>
2026-05-15 00:04:34 +0530

    Udemy

---
+++ Udemy/4-mycv/dist/users/users.controller.d.ts
@@ -0,0 +1,4 @@
+import { createUserDto } from './dtos/create-user.dto';
+export declare class UsersController {
+    createUser(body: createUserDto): void;
+}
--- Udemy/4-mycv/src/main.ts
+++ Udemy/4-mycv/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true //It will remove additional fields from input request body. It will only pass those fields which are present in the DTO.
+    })
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
